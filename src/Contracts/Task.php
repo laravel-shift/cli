d2e447a3006d617a260c2f02394946e22e09e8e3
Jason McCreary <jason@pureconcepts.net>
2023-08-10 12:21:40 -0400

    Remove SDK files

--- src/Contracts/Task.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Shift\Cli\Contracts;
-
-interface Task
-{
-    public function perform(): int;
-}
--- tests/SnapshotState.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Tests;
-
-class SnapshotState
-{
-    public static $purged = false;
-}
